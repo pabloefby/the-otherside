268b55e362a37e0396230a116fb69ecaa866f295
adan <horacio_drbll@hotmail.com>
2025-09-21 19:05:14 -0600

    Se agrego: carpetas pages, components y assets, se modifico el index.css y se creo la pagina de registro con su css

---
+++ front/the-otherside/src/pages/Register.jsx
@@ -0,0 +1,34 @@
+import styles from "./Register.module.css";
+
+function Register() {
+  return (
+    <div className={styles.register}>
+      {/* Elemento de presentación */}
+      <div className={styles.register__presentation}>
+        <h1 className={styles.register__title}>The Otherside</h1>
+      </div>
+
+      {/* Elemento que contiene el formulario */}
+      <div className={styles.register__data}>
+        <form className={styles.register__form}>
+          <a href="" className={`btn ${styles["register__button--back"]}`}>
+            <i class="fa-solid fa-left-long"></i>
+          </a>
+          <h2 className={styles.register__subtitle}>REGISTRAR USUARIO</h2>
+          <label className={styles.register__label}>Nombre de usuario</label>
+          <input className={styles.register__input} type="text" />
+
+          <label className={styles.register__label}>Correo electrónico</label>
+          <input className={styles.register__input} type="email" />
+
+          <label className={styles.register__label}>Contraseña</label>
+          <input className={styles.register__input} type="password" />
+          <button type="submit" className={styles.register__button}>
+            Registrarse
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+export default Register;
