f5bde981d75fddc926f651ad097f04e9a48c9c49
adan <horacio_drbll@hotmail.com>
2025-09-25 12:08:04 -0600

    desarrollo y estilos de la navbar

---
+++ front/the-otherside/src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
+import defaultProfile from "../assets/defaultProfile.png";
+
+export function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <h1 className={styles.navbar__logo}>The Otherside</h1>
+
+      <div className={styles.navbar__categories}>
+        <button className={styles.navbar__button}>MISTERIO</button>
+        <button className={styles.navbar__button}>ALIEN</button>
+        <button className={styles.navbar__button}>BRUJERIA</button>
+        <button className={styles.navbar__button}>LEYENDAS</button>
+        <button className={styles.navbar__button}>CONSPIRACION</button>
+        <button className={styles.navbar__button}>CREEPYPASTA</button>
+        <button className={styles.navbar__button}>FANTASMA</button>
+      </div>
+      <div className={styles.navbar__session}>
+        <Link to="" className={styles["navar__image-link"]}>
+          <img
+            src={defaultProfile}
+            alt="profile"
+            className={styles.navbar__image}
+          />
+        </Link>
+        <div className={styles.navbar__info}>
+          <Link to="" className={styles.navbar__user}>
+            Nombre de usuario
+          </Link>
+          <Link to="/" className={styles.navbar__logOut}>
+            Cerrar sesion
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
--- front/the-otherside/src/pages/Login.jsx
+++ front/the-otherside/src/pages/Login.jsx
@@ -1,74 +1,80 @@
 import { Link, useNavigate } from "react-router-dom";
 import styles from "./Login.module.css";
 
-import {useState } from "react";
-import axios from 'axios';
+import { useState } from "react";
+import axios from "axios";
+import { Navbar } from "../components/Navbar";
 
 function Login() {
-
-  const [nomusuario, setNomusuario] = useState('');
-  const [contra, setContra] = useState('');
+  const [nomusuario, setNomusuario] = useState("");
+  const [contra, setContra] = useState("");
 
   const redirect = useNavigate();
 
   const loginUser = async (e) => {
     e.preventDefault();
 
-    try{
+    try {
+      const respuesta = await axios.post("http://localhost:3001/login-point", {
+        name: nomusuario,
+        passW: contra,
+      });
 
-      const respuesta = await axios.post("http://localhost:3001/login-point",
-        {
-          name: nomusuario,
-          passW: contra
-        }
-      );
-
-      if(respuesta.data.msg ==="LOGIN EXITOSO"){
+      if (respuesta.data.msg === "LOGIN EXITOSO") {
         alert("Bienvenido al culto"); // Loggeo exitoso
-        localStorage.setItem("user",respuesta.data.user);
+        localStorage.setItem("user", respuesta.data.user);
         redirect("/Home");
-      }else if(respuesta.data.msg==="NO ENCONTRADO"){
+      } else if (respuesta.data.msg === "NO ENCONTRADO") {
         alert("Usuario o contraseña incorrecta"); // No encontrado
         e.target.reset();
-      }else if(respuesta.data.msg==="ERROR"){
-        alert("Error en el servidor");  // Error en el servidor
+      } else if (respuesta.data.msg === "ERROR") {
+        alert("Error en el servidor"); // Error en el servidor
         e.target.reset();
       }
-
-    }catch(error){
+    } catch (error) {
       console.log(error);
       alert("Error en la peticion");
     }
-
-  }
+  };
   return (
-    <div className={styles.login}>
-      {/* Elemento de presentación */}
-      <div className={styles.login__presentation}>
-        <h1 className={styles.login__title}>The Otherside</h1>
-      </div>
+    <div className={styles.body}>
+      <Navbar />
+      <div className={styles.login}>
+        {/* Elemento de presentación */}
+        <div className={styles.login__presentation}>
+          <h1 className={styles.login__title}>The Otherside</h1>
+        </div>
 
-      {/* Elemento que contiene el formulario */}
-      <div className={styles.login__data}>
-        <form className={styles.login__form}
-        onSubmit={loginUser}>
+        {/* Elemento que contiene el formulario */}
+        <div className={styles.login__data}>
+          <form className={styles.login__form} onSubmit={loginUser}>
+            <h2 className={styles.login__subtitle}>INICIAR SESION</h2>
+            <label className={styles.login__label}>Nombre de usuario</label>
+            <input
+              className={styles.login__input}
+              type="text"
+              onChange={(e) => {
+                setNomusuario(e.target.value);
+              }}
+            />
 
-          <h2 className={styles.login__subtitle}>INICIAR SESION</h2>
-          <label className={styles.login__label}>Nombre de usuario</label>
-          <input className={styles.login__input} type="text" 
-          onChange={(e) => {setNomusuario(e.target.value)}}/>
+            <label className={styles.login__label}>Contraseña</label>
+            <input
+              className={styles.login__input}
+              type="password"
+              onChange={(e) => {
+                setContra(e.target.value);
+              }}
+            />
 
-          <label className={styles.login__label}>Contraseña</label>
-          <input className={styles.login__input} type="password" 
-          onChange={(e) => {setContra(e.target.value)}}/>
-
-          <Link to="Register" className={styles.login__register}>
-            ¿Aun no formas parte del culto? inicia el ritual
-          </Link>
-          <button type="submit" className={styles.login__button}>
-            Entrar
-          </button>
-        </form>
+            <Link to="Register" className={styles.login__register}>
+              ¿Aun no formas parte del culto? inicia el ritual
+            </Link>
+            <button type="submit" className={styles.login__button}>
+              Entrar
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
