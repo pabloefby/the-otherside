0650b5d541bc88fccdefd770ebde8445736f20a6
adan <horacio_drbll@hotmail.com>
2025-10-01 12:31:23 -0600

    Cambio de estilo Navbar y desarrollo en progreso de la pagina inicio

--- front/the-otherside/src/pages/Home.jsx
+++ front/the-otherside/src/pages/Home.jsx
@@ -1,19 +1,45 @@
+import styles from "./Home.module.css";
 import { Navbar } from "../components/Navbar";
 import { Navigate } from "react-router-dom";
+import skullIcon from "../assets/skullIcon.png";
 
 function Home() {
   const user = localStorage.getItem("user");
 
-  if(!user) 
-    return  <Navigate to="/" replace /> 
+  const skull = (
+    <img src={skullIcon} alt="skullIcon" className={styles.skullIcon} />
+  );
+
+  if (!user) return <Navigate to="/" replace />;
 
   return (
-      <div>
+    <div className={styles.body}>
       <Navbar />
-      <h1>Bienvenido al culto {user}</h1>
+      <div className={styles.home}>
+        <h2 className={styles.home__welcome}>Bienvenido al culto {user}</h2>
+        <div className={styles.home__collage}></div>
+        <div className={styles.home__ranking}>
+          <label className={styles.home__subtitle}>
+            Ciudades con mayor prescencia paranormal
+          </label>
+          <div className={styles["home__ranking-group"]}>
+            <div className={styles["home__ranking-element"]}>
+              <label className={styles["home__city"]}>Monterrey</label>
+              <label className={styles["home__counter"]}>
+                23 publicaciones
+              </label>
+              <div className="styles['home__score']">
+                {skull}
+                {skull}
+                {skull}
+                {skull}
+                {skull}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    
-    
   );
 }
 
