95a48e3e8619e0204471c9a819637facb890d672
adanLap <horacio_drbll@hotmail.com>
2025-10-22 18:07:52 -0600

    home y componente de publicaciones

--- front/the-otherside/src/pages/Home.jsx
+++ front/the-otherside/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import styles from "./Home.module.css";
 import { Navbar } from "../components/Navbar";
+import { PostPreview } from "../components/PostPreview";
 import { Navigate } from "react-router-dom";
 import skullIcon from "../assets/skullIcon.png";
 
@@ -7,7 +8,7 @@ function Home() {
   const user = localStorage.getItem("user");
 
   const skull = (
-    <img src={skullIcon} alt="skullIcon" className={styles.skullIcon} />
+    <img src={skullIcon} alt="skullIcon" className="skullStyle" />
   );
 
   if (!user) return <Navigate to="/" replace />;
@@ -18,8 +19,8 @@ function Home() {
       <div className={styles.home}>
         <h2 className={styles.home__welcome}>Bienvenido al culto {user}</h2>
         <div className={styles.home__collage}></div>
-        <div className={"container"}>
-          <label className={"subtitle"}>
+        <div className="container">
+          <label className="subtitle">
             Ciudades con mayor prescencia paranormal
           </label>
           <div className={styles["home__ranking-group"]}>
@@ -28,7 +29,7 @@ function Home() {
               <label className={styles["home__counter"]}>
                 23 publicaciones
               </label>
-              <div className="styles['home__score']">
+              <div className={styles["home__ranking-score"]}>
                 {skull}
                 {skull}
                 {skull}
@@ -38,8 +39,17 @@ function Home() {
             </div>
           </div>
         </div>
-        <div className={"container"}>
-          <label className={"subtitle"}>Publicaciones mas recientes</label>
+        <div className="container">
+          <div className="topSection">
+          <label className="subtitle">Publicaciones mas recientes</label>
+          <button> <i class="fa-solid fa-plus"></i> Crear publicacion</button>
+          </div>
+          <div className="content">
+            <PostPreview></PostPreview>
+            <PostPreview></PostPreview>
+            <PostPreview></PostPreview>
+
+          </div>
         </div>
       </div>
     </div>
