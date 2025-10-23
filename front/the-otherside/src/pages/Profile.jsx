aa13340dac2eb97c0de03f7930406e51e086746e
RaulTDavila <rtdc03@gmail.com>
2025-10-23 04:16:57 -0600

    diseno base profile

--- front/the-otherside/src/pages/Profile.jsx
+++ front/the-otherside/src/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import styles from "./Profile.module.css";
 import { Navbar } from "../components/Navbar";
+import { PostPreview } from "../components/PostPreview";
 import { Navigate } from "react-router-dom";
 import skullIcon from "../assets/skullIcon.png";
 
@@ -9,7 +10,35 @@ function Profile() {
   return (
     <div className={styles.body}>
       <Navbar />
-      <h1>hola prueba</h1>
+      <div className={styles.profile}>
+        <div className="container">
+          <div className={styles.profile__info}>
+            <label className="subtitle">Informacion Personal</label>
+            <div className={styles.profile__details}>
+              <label className={styles.profile_labelNombre}>{user}</label>
+              <br></br>
+              <label className={styles.profile_label}>Nombre Completo</label>
+              <p className={styles.profile_p}> Raul Tadeo Davila Castro</p>
+              <label className={styles.profile_label}>Correo Electronico</label>
+              <p className={styles.profile_p}>Monterrey</p>
+              <label className={styles.profile_label}>Contraseña</label>
+              <p className={styles.profile_p}> ******** </p>
+            </div>
+          </div>
+        </div>
+            <div className={styles.profile__buttons}>
+            <button className={styles.profile__MisPubli_Button}>Mis Publicaciones</button>
+            <button className={styles.profile__PubliCalif_Button}>Puiblicaciones que has calificado</button>
+            </div>
+        <div className="container">
+            <div className={styles.profile_btnEditYElim}>
+                <button className={styles.profile__edit_Button}>Editar</button>
+                <button className={styles.profile__delete_Button}>Eliminar</button>
+            </div>
+            <PostPreview></PostPreview>
+            <PostPreview></PostPreview>
+        </div>
+      </div>
     </div>
   );
 }
